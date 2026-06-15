ef668263dca20c62765926883f44f41faa34cd58
Peter Khrissanov <pkhrissanov@gmail.com>
2026-06-14 22:37:10 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+
+integer_type = 5
+float_type = 5.2
+bool_type = True
+string_type = "1"
+list_type = [1,2,3]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
