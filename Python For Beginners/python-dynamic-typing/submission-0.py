97490f2edc3a385f4801f94a858fe394a56fc55b
Peter Khrissanov <pkhrissanov@gmail.com>
2026-06-14 22:38:13 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 4.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "ee"
+print(type(variable))
+variable = [1,3]
+print(type(variable))
