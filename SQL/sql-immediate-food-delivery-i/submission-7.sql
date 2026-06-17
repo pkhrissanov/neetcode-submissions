16818b8a0866cf728d0a08507776e97fbb68f085
Peter Khrissanov <pkhrissanov@gmail.com>
2026-06-17 13:58:11 -0700

    Add: sql-immediate-food-delivery-i - submission-7

---
+++ SQL/sql-immediate-food-delivery-i/submission-7.sql
@@ -0,0 +1,8 @@
+
+
+SELECT ROUND((1.0 * SUM(
+    CASE 
+    WHEN order_date=customer_pref_delivery_date THEN 1
+    ELSE 0
+    END)/COUNT(delivery_id)) * 100, 2) AS immediate_percentage 
+FROM delivery 
