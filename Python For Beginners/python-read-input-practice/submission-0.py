1b781e466e958f5c9b6dabd3cda4c3ffb4db07f8
Peter Khrissanov <pkhrissanov@gmail.com>
2026-06-23 13:34:41 -0700

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,10 @@
+def add_two_numbers() -> int:
+    x = [int(x) for x in input().split(",")]
+    return sum(x)
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
